f678e7465d2c475487e39c7057b697b398caf59d
Nick Cooper <nicknack359@gmail.com>
2020-05-19 21:13:16 +1200

    Updated-Array-Input

--- Recipe_Scaler/00_Program.cs
+++ Recipe_Scaler/00_Program.cs
@@ -11,11 +11,12 @@ namespace Recipe_Scaler
 
         
         static float[] measurementVal = new float[20];
-        static float tempVal, ingredNumOut;
-        static string[] measurementUnit = new string[20], /* array for checking if unit is valid */ unitList = new string[] { "c", "l", "ml", "g", "tsp", "tbsp", "kg" };
+        static float tempVal;
+        static int ingredNumOut = 0;
+        static string[] measurementUnit = new string[20], /* array for checking if unit is valid */ 
+            unitList = new string[] { "c", "l", "ml", "g", "tsp", "tbsp", "kg" }, ingredientName = new string[20];
         
-        static string tempUnit, tempValConvert;
-        static string recipeName = "";
+        static string tempUnit, tempValConvert, recipeName, tempIngredName;
         static bool completedInput = false, endOfRecipe = false;
         // function for checking in array for variable
         static void checkArray(Array arrayForCheck, string stringForCheck)
@@ -56,8 +57,30 @@ namespace Recipe_Scaler
             
         }
         // User input function begins here
-        static void input(int ingredNum)
+        static void input(int ingredNum, bool fullExplain)
         {
+            nameFailed:
+            tempIngredName = Console.ReadLine();
+            if(tempIngredName == "done") {endOfRecipe = true; goto recipeEnd; }
+            if(tempIngredName.Length > 0)
+            {
+                ingredientName[ingredNum] = tempIngredName;
+                if (fullExplain)
+                {
+                    Console.WriteLine("Awesome! The ingredient name has been entered correctly!");
+                    Console.WriteLine("Now enter the ingredient amount in decimal form. E.g 1/2 Cup = 0.5");
+                }
+                else
+                {
+                    Console.WriteLine("Name entry successful!");
+                }
+                
+            }
+            else
+            {
+                Console.WriteLine("Oops! It looks like nothing was entered! please try again.");
+                goto nameFailed;
+            }
             completedInput = false;
             while (!completedInput)
             {
@@ -68,47 +91,78 @@ namespace Recipe_Scaler
                 {
                     measurementVal[ingredNum] = tempVal;
                     waitingProceed(3, true);
-                    // Confirmation of success and next instructions
-                    //Console.WriteLine("Great, that was successful!, Now enter the measurement unit");
-                    //Console.WriteLine("in shorthand. E.g: Cups = C, Milliliters = Ml, grams = g etc.");
+                    if (fullExplain)
+                    {
+                        // Confirmation of success and next instructions
+                        Console.WriteLine("Great, that was successful!, Now enter the measurement unit");
+                        Console.WriteLine("in shorthand. E.g: Cups = C, Milliliters = Ml, grams = g etc.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Value entry successful.");
+                    }
+                    
                 }
                 else
                 {
                     Console.WriteLine("Uh Oh! It looks like that wasn't a number! Please try again.");
                 }
-                Console.WriteLine("String: " + tempValConvert + " parsed output: " + tempVal + " successful parsing? " + completedInput);
             }
+            completedInput = false;
             // receiving next user input (unit)
+
+            redoUnit:
             tempUnit = Console.ReadLine();
             tempUnit = tempUnit.ToLower();
-
+            checkArray(unitList, tempUnit);
+            if (completedInput)
+            {
+                Console.WriteLine("Unit entry successful. You may proceed.");
+                unitList[ingredNum] = tempUnit;
+            }
+            else
+            {
+                Console.WriteLine("Sorry, that doesn't look like a unit that I know of, try again!");
+                goto redoUnit;
+            }
+            recipeEnd:;
         }
         // This is the main program
         public static void Main(string[] args)
-        
+
         {
             // First basic instructions
             Console.WriteLine(
                 "This program can covert measurements," +
-                "e.g: Cups to Ml or vice versa, and can also scale recipes up & down." +
-                "Feel free to use it!"
-                );
-            waitingProceed(3,true);
+                "e.g: Cups to Ml or vice versa, and can also scale recipes up & down.");
+            Console.WriteLine("Feel free to use it!");
+            waitingProceed(3, true);
             Console.WriteLine("First off, what is the name of your recipe? ");
             recipeName = Console.ReadLine();
             waitingProceed(2, false);
-            Console.WriteLine("Brilliant! Start off by first entering the ingerdient name." +
-                "After confirmation, please enter the measurement value of that ingredient."
-                );
+            Console.WriteLine("Brilliant! Start off by first entering the ingerdient name.");
+            Console.WriteLine("After confirmation, please enter the measurement value of that ingredient.");
             waitingProceed(1, false);
-            Console.WriteLine("After confirmation that the value was valid, enter the unit of the ingredient");
+            Console.WriteLine("After this, you will need to enter the unit of the ingredient");
             waitingProceed(1, false);
             Console.WriteLine("If you have done so correctly you will get a message stating you may proceed." +
                 " Go ahead, try it!");
             // Ensuring that the user has understood correctly
-            input(0);
-            
-            
+            input(0, true);
+            Console.WriteLine("Great! Now just keep entering ingredients like that!");
+            Console.WriteLine("Once you're done, simply enter the word done. Easy as that!");
+            while (!endOfRecipe)
+            {
+                ingredNumOut++;
+                input(ingredNumOut, false);
+            }
+            for(int loops = 0; loops < ingredientName.Length; loops++)
+            {
+                Console.Write(ingredientName[loops]);
+                Console.Write(measurementVal[loops]);
+                Console.WriteLine(unitList[loops]);
+            }
+            Console.ReadLine();
         }
     }
 }
